72faafd22f5c4b1082749fba0d26c88b6b1da1d8
Adriano Rodrigues <111815427+adrianorody@users.noreply.github.com>
2022-08-24 20:50:55 -0300

    Add files via upload

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,12 @@
+let nome = "Adriano"
+let email = "adrianomesenger@gmail.com"
+let profissao = "gestor"
+let idade = 40
+let lugar = "Israel"
+let mensagem = "Olá, " + nome + "! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para " + lugar + ". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem!"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
+console.log(mensagem)
